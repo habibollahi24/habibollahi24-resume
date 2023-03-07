6a18f62518b10f33579fa1bd20e24a04441900cf
mohammad <habibollahi24@gmail.com>
2023-03-07 11:27:53 +0330

    fixed responsive bug

--- src/components/shared/breadCramp.jsx
+++ src/components/shared/breadCramp.jsx
@@ -5,7 +5,7 @@ const BreadCramp = () => {
    const { pathname } = useLocation();
    const { slug } = useParams();
    return (
-      <div className="mb-8 md:ml-6 p-4 border-2 rounded-md text-gray-700 dark:text-gray-300">
+      <div className="mb-8 md:ml-6 p-3 w-3/4 md:w-full border-2 rounded-md text-gray-700 dark:text-gray-300">
          <Link to={`/${pathname.split("/")[1]}`}>{pathname.split("/")[1]}</Link>
          <span className="text-2xl align-middle mx-2">{`>`}</span>
          <span>{slug}</span>
--- src/components/shared/titleSection.jsx
+++ src/components/shared/titleSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TitleSection = ({ children, classes }) => {
    return (
       <div
-         className={`text-gray-700 dark:text-gray-200 text-xl md:text-3xl  text-center md:text-left md:pl-6 ${classes}`}
+         className={`text-gray-700 dark:text-gray-200 text-2xl md:text-3xl  text-center md:text-left md:pl-6 ${classes}`}
       >
          {children}
       </div>
