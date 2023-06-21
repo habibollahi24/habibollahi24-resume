bcafbdcf7b3d40e3e69e2b906f10807767a34d47
habibollahi24 <habibollahi24@gmail.com>
2023-06-21 16:26:59 +0330

    add todo db train

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-   return (
-      <footer className="m-6 text-gray-700 bg-gray-300 dark:bg-dark-300 dark:text-gray-400 absolute bottom-16 md:bottom-5 p-1 rounded-sm">
-         <p className="">
-            UX-UI-Develop: <br /> Habibollahi24 | 2023 <br />{" "}
-            <a
-               href="https://github.com/habibollahi24/habibollahi24-resume"
-               target="_blank"
-               className="underline"
-            >
-               GitHub source
-            </a>
-         </p>
-      </footer>
-   );
+  return (
+    <footer className=" text-center md:text-right text-gray-700  dark:bg-dark-300 dark:text-gray-400    p-5 ">
+      <p className="">
+        UX-UI-Develop: <br /> Habibollahi24 | 2023 <br />{" "}
+        <a
+          href="https://github.com/habibollahi24/habibollahi24-resume"
+          target="_blank"
+          className="underline"
+        >
+          GitHub source
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/trains.jsx
+++ src/pages/trains.jsx
@@ -7,21 +7,21 @@ import useFilter from "./../hooks/useFilter";
 import SelectOption from "../components/shared/selectOption";
 
 const TrainPage = () => {
-   const { search, filteredTrains, onSelectHandler } = useFilter();
+  const { search, filteredTrains, onSelectHandler } = useFilter();
 
-   return (
-      <Layout>
-         <div className="flex justify-between items-end md:items-center mb-6 px-4">
-            <TitleSection classes="">All Trains</TitleSection>
-            <SelectOption search={search} onSelectHandler={onSelectHandler} />
-         </div>
-         <div className="grid grid-cols-1  sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-3 gap-x-5  px-4 mb-12 ">
-            {filteredTrains.map((train) => {
-               return <Cart key={train.id} cartData={train} />;
-            })}
-         </div>
-      </Layout>
-   );
+  return (
+    <Layout>
+      <div className="flex justify-between items-end md:items-center mb-6 px-4">
+        <TitleSection classes="">All Trains</TitleSection>
+        <SelectOption search={search} onSelectHandler={onSelectHandler} />
+      </div>
+      <div className="grid grid-cols-1  sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-3 gap-x-5  px-4 mb-0 ">
+        {filteredTrains.map((train) => {
+          return <Cart key={train.id} cartData={train} />;
+        })}
+      </div>
+    </Layout>
+  );
 };
 
 export default TrainPage;
